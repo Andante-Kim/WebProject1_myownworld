d99ac04663d458cd335df9a7c9220fd8305f743d
MinJeong Kim <andante.mjkim@gmail.com>
2024-10-01 23:22:30 +0900

    Commit3

--- mysite/myownworld/views.py
+++ mysite/myownworld/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
-from django.http import HttpResponseNotAllowed
+#from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -25,6 +26,7 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'myownworld/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     myownworld 답변등록
@@ -34,19 +36,23 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('myownworld:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'myownworld/question_detail.html', context)
+
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid(): # 폼이 유효하다면
             question = form.save(commit=False) # 임시 저장하여 question 객체를 리턴받는다.
+            question.author = request.user # author 속성에 로그인 계정 저장
             question.create_date = timezone.now() # 실제 저장을 위해 작성일시를 설정한다.
             question.save() # 데이터를 실제로 저장한다.
             return redirect('myownworld:index')
