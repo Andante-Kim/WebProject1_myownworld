457415e9f356882fec660671b92c51de380e9167
MinJeong Kim <andante.mjkim@gmail.com>
2024-10-08 17:08:36 +0900

    Commit_24.10.08(basic_end)

--- mysite/myownworld/templatetags/myownworld_filter.py
+++ mysite/myownworld/templatetags/myownworld_filter.py
@@ -1,7 +1,15 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def sub(value, org):
     return value - org
+
+# 마크다운 필터 등록
+@register.filter
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
